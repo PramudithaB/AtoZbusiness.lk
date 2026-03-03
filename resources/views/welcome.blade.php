e66e09e9ef394af1f4c9f0bb0bdddd986ff4ba2a
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2026-03-03 12:33:21 +0530

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,7 +213,7 @@
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#institutes">Feedback</a></li>
-        <li><a href="{{ route('login') }}" style="background: white; color: #001a4d;">Login</a></li>
+        {{-- <li><a href="{{ route('login') }}" style="background: white; color: #001a4d;">Login</a></li> --}}
       </ul>
     </div>
   </nav>
